225681abe45dd371f55bc867f687ce7b63df9321
LCA675 <lauracalleanguita@gmail.com>
2025-01-31 13:55:15 +0100

    Ejercicio 9 terminado

--- ejercicio9.py
+++ ejercicio9.py
@@ -1,10 +1,16 @@
-alumnos=["Laura":8, "Alejandro":10, "Cristóbal":7, "Triana":8]
-def notas_promedio(alumnos):
-    promedio=alumnos.values/len(alumnos)
-    mejor_alumno=max(alumnos, valor=alumnos.get)
-    return promedio
-    return mejor_alumno
+alumnos = {"Laura": 8, "Alejandro": 10, "Cristóbal": 7, "Triana": 6}
 
-promedio, mejor_alumno=notas_promedio(alumnos)
-print(f"El mejor alumno es:", mejor_alumno)
-print(f"El promedio es: ", promedio)
+def notas_promedio(alumnos):
+    nota_max = 0
+    alumno_max = ""
+    promedio = 0
+    for nombre, nota in alumnos.items():
+        if nota > nota_max:
+            nota_max = nota
+            alumno_max = nombre
+        promedio += nota / len(alumnos)
+        
+    return f"Promedio: {promedio:.2f}, Nota máxima: {nota_max}, Mejor estudiante: {alumno_max}"
+
+resultado = notas_promedio(alumnos)
+print(resultado)
